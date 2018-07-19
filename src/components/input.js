adf625945c14d86921c002225153f5a6c44a24bb
NuclearNathan <20ntoone@gmail.com>
2018-07-19 10:11:03 -0600

    Chanded stuff

--- src/components/input.js
+++ src/components/input.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Input = ({title, state, name}, onChange, index) => {
   return (
-    <div key={state} className="input">
-      <label></label>
+    <div key={index} className="input">
+      <label className="input__number">{index + 1}</label>
       <input name={name} value={state}> onChange={onChange}/>
       <label className="input__title">{title}</label>
     </div>
